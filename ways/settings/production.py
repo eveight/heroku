53647983811d16af3437fafdf1fbb1db08f2b4a1
Veliev Elnur <bizelnur@gmail.com>
2020-09-15 21:36:25 +0300

    commit for Heroky

--- ways/settings/production.py
+++ ways/settings/production.py
@@ -26,13 +26,13 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'r31w$k!4_jx6^r#7*#!dj-a-v*xmgc9@*#95lvbdnyk+(-nrr6'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'r31w$k!4_jx6^r#7*#!dj-a-v*xmgc9@*#95lvbdnyk+(-nrr6'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://waysing.herokuapp.com/']
 
 
 # Application definition
